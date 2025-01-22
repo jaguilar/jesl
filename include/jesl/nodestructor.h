0918c4fa739b67e0c65ae1c2aa77a71d96329d6c
James Aguilar <aguilar.james@gmail.com>
2025-01-21 21:48:23 -0500

    Add nodestructor.

---
+++ include/jesl/nodestructor.h
@@ -0,0 +1,50 @@
+#ifndef JESL_NODESTRUCTOR_H
+#define JESL_NODESTRUCTOR_H
+
+#include <new>
+
+namespace jesl {
+
+// This funnily-named class is for putting objects that require construction
+// into a static/global variable. It's not possible to delete the object
+// once it has been constructed.
+template <typename T>
+class NoDestructor {
+ public:
+  NoDestructor() {}
+
+  NoDestructor(const NoDestructor&) = delete;
+  NoDestructor& operator=(const NoDestructor&) = delete;
+  NoDestructor(NoDestructor&&) = delete;
+  NoDestructor& operator=(NoDestructor&&) = delete;
+
+  template <typename... Args>
+  void Init(Args&&... args) {
+    new (storage_) T(std::forward<Args>(args)...);
+#ifndef NDEBUG
+    initialized_ = true;
+#endif
+  }
+
+  T* operator->() { return &get(); }
+  T& operator*() { return get(); }
+  operator T*() { return &get(); }
+  operator T&() { return get(); }
+
+ private:
+  T& get() {
+#ifndef NDEBUG
+    assert(initialized_);
+#endif
+    return *std::launder(reinterpret_cast<T*>(storage_));
+  }
+
+  alignas(alignof(T)) char storage_[sizeof(T)];
+#ifndef NDEBUG
+  bool initialized_ = false;
+#endif
+};
+
+}  // namespace jesl
+
+#endif
