ef9db1497ce9657f108aaa86d9c3ed15cee5ef0c
James Aguilar <aguilar.james@gmail.com>
2026-01-24 23:34:14 -0700

    Add dummy header to make arduino-cli happy.

---
+++ src/jesl.h
@@ -0,0 +1,11 @@
+#ifndef JESL_H
+#define JESL_H
+
+#include "jesl/cleanup.h"
+#include "jesl/esp_check.h"
+#include "jesl/freertos/mutex.h"
+#include "jesl/freertos/queue.h"
+#include "jesl/nodestructor.h"
+#include "jesl/ring.h"
+
+#endif
